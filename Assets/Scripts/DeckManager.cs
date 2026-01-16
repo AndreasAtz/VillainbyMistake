ffdeaf363fc244b88ac678c1f78d4bdf9471eb04
JstRene <rene.uhlig@stud.fh-campuswien.ac.at>
2026-01-16 20:52:58 +0100

    Fully Functioning Deck
    Added HandDisplay, Deck Display and Draw Logic, fixed all older Bugs. Cards are pretty much done, only visually appealing things are missing

---
+++ Assets/Scripts/DeckManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using VillainByMistake;
+using VillainByMistake.Cards;
+
+public class DeckManager : MonoBehaviour
+{
+    public List<Card> allCards = new List<Card>();
+
+    private int currentIndex = 0;
+
+    void Start()
+    {
+        //Load all cards from Resources
+        Card[] cards = Resources.LoadAll<Card>("Cards");
+
+        //Add the loaded cards to the allCards list
+        allCards.AddRange(cards);
+    } 
+
+    public void DrawCard(HandManager handManager){
+        if (allCards.Count == 0)
+            return;
+        
+        Card nextCard = allCards[currentIndex];
+        handManager.AddCardToHand(nextCard);
+        currentIndex = (currentIndex + 1) % allCards.Count;
+        
+    }
+    
+}
---
+++ Assets/Scripts/HandManager.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using VillainByMistake;
+using VillainByMistake.Cards;
+
+public class HandManager : MonoBehaviour
+{
+    public DeckManager deckManager;
+    public GameObject cardPrefab;
+    public Transform handTransform;
+    public float fanSpread = 7.5f;
+    public float cardSpacing = 200f;
+    public float verticalSpacing = 100f;
+    private List<GameObject> cardsInHand = new List<GameObject>();
+
+    void Start()
+    {
+       
+    }
+
+    public void AddCardToHand(Card cardData)
+    {
+        GameObject newCard = Instantiate(cardPrefab, handTransform.position, Quaternion.identity, handTransform);
+        cardsInHand.Add(newCard);
+
+        //Set the Card Data of the instantiated card
+        newCard.GetComponent<CardDisplay>().cardData = cardData;
+        
+        UpdateHandVisuals();
+    }
+
+    void Update()
+    {
+        UpdateHandVisuals();
+    }
+
+    private void UpdateHandVisuals()
+    {
+        int cardCount = cardsInHand.Count;
+
+        if (cardCount == 1)
+        {
+            cardsInHand[0].transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+            cardsInHand[0].transform.localPosition = new Vector3(0f, 0f, 0f);
+            return;
+        }
+
+        for (int i = 0; i < cardCount; i++)
+        {
+            float rotationAngle = (fanSpread * (i - (cardCount - 1) / 2f));
+            cardsInHand[i].transform.localRotation = Quaternion.Euler(0f, 0f, rotationAngle);
+
+            float horizontalOffset = (cardSpacing * (i - (cardCount - 1) / 2f));
+
+            float normalizedPosition = (2f * i / (cardCount - 1)) - 1f;
+            float verticalOffset = verticalSpacing * (1f - normalizedPosition * normalizedPosition);
+            cardsInHand[i].transform.localPosition = new Vector3(horizontalOffset, verticalOffset, 0f);
+        }
+    }
+}
